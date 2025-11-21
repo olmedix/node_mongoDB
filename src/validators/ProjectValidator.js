64aafc6944d62ad217eafd2cdf1fb699b5edc741
olmedix <olmedix@gmail.com>
2025-11-21 16:02:10 +0100

    update projects OK

--- src/validators/ProjectValidator.js
+++ src/validators/ProjectValidator.js
@@ -14,34 +14,36 @@ export async function validateProject(data, mongoUser) {
 
   // DESCRIPTION (opcional, pero si viene debe tener mínimo 8)
   if ("description" in data) {
-    if (typeof data.description !== "string" || data.description.trim().length < 8) {
+    if (
+      typeof data.description !== "string" ||
+      data.description.trim().length < 8
+    ) {
       errors.description = "La descripción debe tener al menos 8 caracteres";
     }
   }
 
   // OWNER ID
-  if (!data.ownerId) {
-    errors.ownerId = "ownerId es obligatorio";
-  } else {
-
-    // ObjectId válido
-    
-    if (!ObjectId.isValid(data.ownerId)) {
-      errors.ownerId = "ownerId debe ser un ObjectId válido";
+  if ("ownerId" in data) {
+    if (!data.ownerId) {
+      errors.ownerId = "ownerId es obligatorio";
     } else {
-      // Verificar que existe en DB
-      const user = await mongoUser.findById(data.ownerId);
+      // ObjectId válido
 
-      if (!user) {
-        errors.ownerId = "El ownerId no corresponde a ningún usuario";
+      if (!ObjectId.isValid(data.ownerId)) {
+        errors.ownerId = "ownerId debe ser un ObjectId válido";
+      } else {
+        // Verificar que existe en DB
+        const user = await mongoUser.findById(data.ownerId);
+
+        if (!user) {
+          errors.ownerId = "El ownerId no corresponde a ningún usuario";
+        }
       }
     }
-      
   }
-    
 
   return {
     isValid: Object.keys(errors).length === 0,
-    errors
+    errors,
   };
 }
