78074875edf7cc10610fa43bc7de5ff198767b9d
olmedix <olmedix@gmail.com>
2025-11-25 16:23:15 +0100

    test para user update OK

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -9,6 +9,7 @@ let server;
 
 beforeAll(async () => {
   await mongoDBUser.deleteByEmail("test@login.com");
+  await mongoDBUser.deleteByEmail("admin@login.com");
   await mongoDBUser.deleteByEmail("anthony@mail.com");
 
   await mongoDBUser.createUser({
@@ -16,7 +17,15 @@ beforeAll(async () => {
     surname: "Login",
     email: "test@login.com",
     password: "123456",
-    role: "User",
+    role: "Guest",
+  });
+
+  await mongoDBUser.createUser({
+    name: "admin",
+    surname: "admin",
+    email: "admin@login.com",
+    password: "123456",
+    role: "Admin",
   });
 });
 
@@ -34,98 +43,252 @@ afterAll(async () => {
   }
 });
 
-describe("User: Store" , () => {
-
-  test("Debe devolver 200 si se ha insertado un usuario en la BBDD" , async () => {
+describe("User: Store", () => {
+  test("Debe devolver 200 si se ha insertado un usuario en la BBDD", async () => {
     const res = await request(server)
-    .post("/users")
-    .send({
-    name: "Andrés",
-    surname: "Santos",
-    email: "anthony@mail.com",
-    password: "123456",
-    role: "Guest"
-  })
-  .expect(201);
-
-  expect(res.body.ok).toBe(true);
-  expect(res.body.data).toBeDefined();
-  expect(res.body.error).toBeNull();
-
-  })
-
-  test("Debe devolver 400 si falta un campo obligatorio" , async () => {
-    const res = await request(server)
-    .post("/users")
-    .send({
-    //name: "Andrés",
-    surname: "Santos",
-    email: "anthony@mail.com",
-    password: "123456",
-    role: "Guest"
-  })
-  .expect(400);
-
-  expect(res.body.ok).toBe(false);
-  expect(res.body.data).toBeNull();
-  expect(res.body.error).toBe("name, surname, email y password son obligatorios");
-  })
-
-  test("Debe devolver 409 si el email ya está registrado" , async () => {
-    const res = await request(server)
-    .post("/users")
-    .send({
-    name: "Andrés",
-    surname: "Santos",
-    email: "anthony@mail.com",
-    password: "123456",
-    role: "Guest"
-  })
-  .expect(409);
-
-  expect(res.body.ok).toBe(false);
-  expect(res.body.data).toBeNull();
-  expect(res.body.error).toBe("El email ya está registrado");
-  })
-
-
-});
-
-describe("User: show/:email", () => {
-
-  test("Debe devolver 400 si falta el parámetro email", async () => {
-
-    const res = await request(server)
-    .get("/users/")
-    .expect(400);
-
-    expect(res.body.ok).toBe(false);
-    expect(res.body.data).toBeNull();
-    expect(res.body.error).toBe("Falta parámetro email");
-  })
-
-  test("Debe devolver 404 si el email no está en la BBDD", async () => {
-
-    const res = await request(server)
-    .get("/users/test1111@login.com")
-    .expect(404);
-
-    expect(res.body.ok).toBe(false);
-    expect(res.body.data).toBeNull();
-    expect(res.body.error).toBe("Usuario no encontrado");
-  })
-
-  test("Debe devolver 200 si encuentra el usuario mediante email", async () => {
-
-    const res = await request(server)
-    .get("/users/test@login.com")
-    .expect(200);
+      .post("/users")
+      .send({
+        name: "Andrés",
+        surname: "Santos",
+        email: "anthony@mail.com",
+        password: "123456",
+        role: "Guest",
+      })
+      .expect(201);
 
     expect(res.body.ok).toBe(true);
     expect(res.body.data).toBeDefined();
     expect(res.body.error).toBeNull();
-  })
+  });
 
+  test("Debe devolver 400 si falta un campo obligatorio", async () => {
+    const res = await request(server)
+      .post("/users")
+      .send({
+        //name: "Andrés",
+        surname: "Santos",
+        email: "anthony@mail.com",
+        password: "123456",
+        role: "Guest",
+      })
+      .expect(400);
+
+    expect(res.body.ok).toBe(false);
+    expect(res.body.data).toBeNull();
+    expect(res.body.error).toBe(
+      "name, surname, email y password son obligatorios"
+    );
+  });
+
+  test("Debe devolver 409 si el email ya está registrado", async () => {
+    const res = await request(server)
+      .post("/users")
+      .send({
+        name: "Andrés",
+        surname: "Santos",
+        email: "anthony@mail.com",
+        password: "123456",
+        role: "Guest",
+      })
+      .expect(409);
+
+    expect(res.body.ok).toBe(false);
+    expect(res.body.data).toBeNull();
+    expect(res.body.error).toBe("El email ya está registrado");
+  });
+});
+
+// delete("/users/:id", protect(authBearer, requireSelfOrAdmin, userControllerInstance.destroy));
+describe("User: Destroy", () => {
+  let user, userAdmin;
+
+  beforeEach(async () => {
+    await mongoDBUser.createUser({
+      name: "delete",
+      surname: "Logdeletein",
+      email: "delete@login.com",
+      password: "123456",
+      role: "Guest",
+    });
+
+    await mongoDBUser.createUser({
+      name: "delete",
+      surname: "Logdeletein",
+      email: "admin@login.com",
+      password: "123456",
+      role: "Admin",
+    });
+
+    user = await request(server)
+      .post("/login")
+      .send({
+        email: "delete@login.com",
+        password: "123456",
+      })
+      .expect(200);
+
+    userAdmin = await request(server)
+      .post("/login")
+      .send({
+        email: "admin@login.com",
+        password: "123456",
+      })
+      .expect(200);
+  });
+
+  test("Debe devolver 200 si es el propio usuario ", async () => {
+    const res = await request(server)
+      .delete(`/users/${user.body.data.user.id}`)
+      .set("Authorization", `Bearer ${user.body.data.token}`)
+      .expect(200);
+
+    expect(res.body.ok).toBe(true);
+    expect(res.body.data).not.toBeNull();
+    expect(res.body.error).toBeNull();
+  });
+
+  test("Debe devolver 200 si es un usuario administrador ", async () => {
+    const res = await request(server)
+      .delete(`/users/${user.body.data.user.id}`)
+      .set("Authorization", `Bearer ${userAdmin.body.data.token}`)
+      .expect(200);
+
+    expect(res.body.ok).toBe(true);
+    expect(res.body.data).not.toBeNull();
+    expect(res.body.error).toBeNull();
+  });
+
+  test("Debe devolver 403 si no se ha proporcionadi ID ", async () => {
+    const res = await request(server)
+      .delete(`/users/`)
+      .set("Authorization", `Bearer ${user.body.data.token}`)
+      .expect(403);
+
+    expect(res.body.ok).toBe(false);
+    expect(res.body.data).toBeNull();
+    expect(res.body.error).toBe("No tienes permisos para esta acción");
+  });
+
+  test("Debe devolver 401 si es el usuario no está autorizado", async () => {
+    const res = await request(server)
+      .delete(`/users/${user.body.data.user.id}`)
+      //.set("Authorization", `Bearer ${user.body.data.token}`)
+      .expect(401);
+
+    expect(res.body.ok).toBe(false);
+    expect(res.body.data).toBeNull();
+    expect(res.body.error).not.toBeNull();
+  });
+});
+
+describe("User: Update", () => {
+  let user, userAdmin;
+
+  beforeEach(async () => {
+    user = await request(server)
+      .post("/login")
+      .send({
+        email: "test@login.com",
+        password: "123456",
+      })
+      .expect(200);
+
+    userAdmin = await request(server)
+      .post("/login")
+      .send({
+        email: "admin@login.com",
+        password: "123456",
+      })
+      .expect(200);
+  });
+
+  test("Debe devolver 200 si es el propio usuario ", async () => {
+    const res = await request(server)
+      .put(`/users/${user.body.data.user.id}`)
+      .set("Authorization", `Bearer ${user.body.data.token}`)
+      .send({name:"Usuario actualizado"})
+      .expect(200);
+
+    expect(res.body.ok).toBe(true);
+    expect(res.body.data).not.toBeNull();
+    expect(res.body.error).toBeNull();
+  });
+
+  test("Debe devolver 200 si es un usuario administrador ", async () => {
+    const res = await request(server)
+      .put(`/users/${user.body.data.user.id}`)
+      .set("Authorization", `Bearer ${userAdmin.body.data.token}`)
+      .send({surname:"User update"})
+      .expect(200);
+
+    expect(res.body.ok).toBe(true);
+    expect(res.body.data).not.toBeNull();
+    expect(res.body.error).toBeNull();
+  });
+
+  test("Debe devolver 403 si no se ha proporcionadi ID ", async () => {
+    const res = await request(server)
+      .put(`/users/`)
+      .set("Authorization", `Bearer ${user.body.data.token}`)
+      .expect(403);
+
+    expect(res.body.ok).toBe(false);
+    expect(res.body.data).toBeNull();
+    expect(res.body.error).toBe("No tienes permisos para esta acción");
+  });
+
+  test("Debe devolver 401 si es el usuario no está autorizado", async () => {
+    const res = await request(server)
+      .put(`/users/${user.body.data.user.id}`)
+      //.set("Authorization", `Bearer ${user.body.data.token}`)
+      .expect(401);
+
+    expect(res.body.ok).toBe(false);
+    expect(res.body.data).toBeNull();
+    expect(res.body.error).not.toBeNull();
+  });
+
+
+  test("Debe devolver 400", async () => {
+    const res = await request(server)
+      .put(`/users/${user.body.data.user.id}`)
+      .set("Authorization", `Bearer ${userAdmin.body.data.token}`)
+      .send({apellidos:"User update"})
+      .expect(400);
+
+    expect(res.body.ok).toBe(false);
+    expect(res.body.data).toBeNull();
+    expect(res.body.error).not.toBeNull();
+  });
+  
+});
+
+describe("User: Show/:email", () => {
+  test("Debe devolver 400 si falta el parámetro email", async () => {
+    const res = await request(server).get("/users/").expect(400);
+
+    expect(res.body.ok).toBe(false);
+    expect(res.body.data).toBeNull();
+    expect(res.body.error).toBe("Falta parámetro email");
+  });
+
+  test("Debe devolver 404 si el email no está en la BBDD", async () => {
+    const res = await request(server)
+      .get("/users/test1111@login.com")
+      .expect(404);
+
+    expect(res.body.ok).toBe(false);
+    expect(res.body.data).toBeNull();
+    expect(res.body.error).toBe("Usuario no encontrado");
+  });
+
+  test("Debe devolver 200 si encuentra el usuario mediante email", async () => {
+    const res = await request(server).get("/users/test@login.com").expect(200);
+
+    expect(res.body.ok).toBe(true);
+    expect(res.body.data).toBeDefined();
+    expect(res.body.error).toBeNull();
+  });
 });
 
 describe("Auth: Login", () => {
@@ -197,5 +360,3 @@ describe("Auth: Logout", () => {
     expect(tokenBlackList.token).toBe(token);
   });
 });
-
-
